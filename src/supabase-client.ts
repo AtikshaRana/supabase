82ea1e3dd9fed329964998a5f73c99e960438bbe
Atiksha <atiksha@cleardigital.com>
2025-04-29 19:11:06 +0530

    Added the task bar

---
+++ src/supabase-client.ts
@@ -0,0 +1,5 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient("https://gylwmaumpatptpkmtuez.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd5bHdtYXVtcGF0cHRwa210dWV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU0OTM0NDMsImV4cCI6MjA2MTA2OTQ0M30.NibouwSWV7oS2RSJ9GGg8qjMDFqqFui0ZFZ3N4FPv-8"
+)
